ee17e70e3408bbb1920b6a0b36346567a0b12ac4
CyberWarr10r <64969369+L3arnH0wT0H4ck@users.noreply.github.com>
2023-01-02 13:38:31 -0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,12 +1,20 @@
 def menu():
-    user_choice = -1
-    print('=========================================================================')
-    while not(user_choice >=0 and user_choice < 4):
-        user_choice = int(input("|\t\t\tEnter : \n\
-|>1.Only calculate the net and the host id of the network.\n\
-|>2.Divide the network into x given subnets(FLSM).\n\
-|>3.Divide the network based on the number of users(VLMS).\n\
-|>Thanks to elucidate your choice (you have to just enter 1, 2 or 3) : "))
-    print('|========================================================================')
-    return user_choice
-
+    print("""
+ ___   _      ___   _      ___   _      ___   _      ___   _
+[(_)] |=|    [(_)] |=|    [(_)] |=|    [(_)] |=|    [(_)] |=|
+ '-`  |_|     '-`  |_|     '-`  |_|     '-`  |_|     '-`  |_|
+    /PC1/        /PC2/       /PC3/       /PC4/       /PC5/     Author/0|I\/3R
+    |____________|__________|___________|___________|_______________|___________Subnet/Calculator
+                        |            |            |                 |
+                    ___  \_      ___  \_      ___  \_           version 1.0  
+                   [(_)] |=|    [(_)] |=|    [(_)] |=|
+                    '-`  |_|     '-`  |_|     '-`  |_|
+                    /PC6/        /PC7/        /PC8/
+    """)
+    print("""
+|=======================================================================
+|\t\t\tEnter
+|(1) Calculate the net and the host id of the network.
+|(2) Divide the network into x given subnets(FLSM).
+|(3) Divide the network based on the number of users(VLSM).
+|=======================================================================""")

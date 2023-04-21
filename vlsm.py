cadc759acd7a26251d30a6956bf5f2c8308f96a0
Olivier Konaté <64969369+0liverFlow@users.noreply.github.com>
2023-04-21 13:34:19 -0700

    Update and rename VLSM.py to vlsm.py

--- vlsm.py
+++ vlsm.py
@@ -1,13 +1,15 @@
-from GetNetHost import NetHostId
+import sys
+
+from get_net_host_id import NetHostId
+
 
 class VariableLengthSubnetMask(NetHostId):
     def __init__(self, ip_address, default_subnet_mask, number_of_users_list):
         super().__init__(ip_address, default_subnet_mask)
-        #self.ip = ip_address
         self.default_network_address = ''
         self.number_of_users_list = number_of_users_list
-        #self.default_subnet_mask = default_subnet_mask
         self.subnet_significant_byte = None
+        #self.total_number_of_hosts_required = 0
     
     def get_new_subnet_mask(self, subnet):
         x = 1
@@ -35,3 +37,31 @@ class VariableLengthSubnetMask(NetHostId):
             self.ip = self.ip_separator.join(network_address)
         self.ip_cidr = self.ip + '/' + str(self.default_subnet_mask)
         self.machines = 2 ** (32 - self.default_subnet_mask) - 2
+
+    def check_number_of_available_hosts(self):
+        # Check if the subnet can be divided between the number of hosts specified by the user
+        specified_hosts = 0
+        x = 1
+        for users in self.number_of_users_list:
+            while x * 2 < users:
+                x *= 2
+            specified_hosts += x*2
+            x = 1
+        if specified_hosts > self.hosts:
+            #self.total_number_of_hosts_required = specified_hosts
+            return True
+        
+    def get_vlsm(self, number_of_subnets):
+        number_of_subnets
+        for subnet_number in range(number_of_subnets):
+            display_header = "SUBNET - " + str(self.number_of_users_list[subnet_number])
+            if subnet_number == 0:
+                self.get_new_subnet_mask(subnet_number)
+            self.subnet_significant_byte = self.default_subnet_mask // 8
+            n_a, b_a, f_a, l_a = self.get_network_broadcast_first_last_addresses()
+            self.set_network_broadcast_first_last_addresses(n_a, b_a, f_a, l_a)
+            self.hosts = 2**(32-self.default_subnet_mask)-2
+            self.display_network_info(display_header)
+            if subnet_number < number_of_subnets-1:
+                self.get_new_subnet_mask(subnet_number + 1)
+                self.get_new_default_network_address()
